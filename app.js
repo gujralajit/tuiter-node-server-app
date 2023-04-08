1bf6f4db1d6df4515d3fa98b8f1899cbcee95a32
gujralajit <gujralajit@gmail.com>
2023-04-08 15:25:01 -0400

    Updated environment variable.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tuiter' || process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 // mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 const app = express()
